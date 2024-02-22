043c31050c0c7d7a5e588618dca85c7a8f4209d7
Hamza#1 <hamzasamara9@gmail.com>
2024-02-22 10:42:14 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String [] args){
         System.out.println("Hmaza 1 ");
         System.out.println("Test 01");
+        System.out.println("Test 02");
+
     }
 }
